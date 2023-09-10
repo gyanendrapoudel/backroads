1cf4ead5c046e94cb2344baffeae018d51906de3
gyanendrapoudel <38423428+gyanendrapoudel@users.noreply.github.com>
2023-09-10 12:41:41 +1000

    Update data.js

--- src/data.js
+++ src/data.js
@@ -6,13 +6,13 @@ import tour4 from './images/tour-4.jpeg'
 export const pageLinks = [
   { id: 1, href: '#home', text: 'home' },
   { id: 2, href: '#about', text: 'about' },
-  { id: 1, href: '#services', text: 'services' },
-  { id: 1, href: '#tours', text: 'tours' },
+  { id: 3, href: '#services', text: 'services' },
+  { id: 4, href: '#tours', text: 'tours' },
 ]
 export const socialLinks = [
   { id: 1, href: 'https://www.twitter.com', icon: 'fab fa-facebook' },
-  { id: 1, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
-  { id: 1, href: 'https://www.twitter.com', icon: 'fab fa-squarespace' },
+  { id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
+  { id: 3, href: 'https://www.twitter.com', icon: 'fab fa-squarespace' },
 ]
 
 export const services = [
